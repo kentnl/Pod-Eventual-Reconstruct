4cd3f9eb56d781a8df313a078b196bb74d0d7ec5
Kent Fredric <kentfredric@gmail.com>
2015-06-22 13:58:32 +1200

    Build results of 02d5512 (on master)

--- lib/Pod/Eventual/Reconstruct/LazyCut.pm
+++ lib/Pod/Eventual/Reconstruct/LazyCut.pm
@@ -226,7 +226,7 @@ Additionally, this module will warn if elements are posted to it in ways that ar
 
 =back
 
-The specific behaviour occurred when hitting these errors can be customised via sub-classing,
+The specific behavior occurred when hitting these errors can be customized via sub-classing,
 and overriding L</write_text_outside_pod> and L</write_nonpod_inside_pod>
 
 =head1 METHODS
